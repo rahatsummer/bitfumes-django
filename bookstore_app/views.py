e0f41048684d0ec54a09df1d6eede1d90e1e8289
rahatsummer <rahatsummer@gmail.com>
2024-04-19 21:19:15 +0600

    load data from json file

--- bookstore_app/views.py
+++ bookstore_app/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import json
 
+
+booksData = open(
+    r'E:\Tutorial\django\bitfumes\django-course\bookstore_app\books.json').read()
+
+
+data = json.loads(booksData)
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("you are in book store index page")
+    context = {'books': data}
+    return render(request, 'bookstore/index.html', context)
